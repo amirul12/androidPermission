363f62ce954e649f36123f0d7aaeafbda41729ce
Shadman Rashik Ahmed <shadmanrashik@gmail.com>
2019-03-13 14:46:43 +0600

    easy permission

---
+++ app/src/main/java/com/amirul/mandsit/MainActivity.java
@@ -0,0 +1,88 @@
+package com.amirul.mandsit;
+
+import android.Manifest;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.List;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.AppSettingsDialog;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class MainActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks{
+
+    private static final String TAG = "MainActivity";
+    private Button btnOpenCamera;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnOpenCamera = (Button)findViewById(R.id.btn_Camera);
+        btnOpenCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
+            }
+        });
+
+
+    }
+
+    @AfterPermissionGranted(123)
+    private void openCamera() {
+
+        Log.i(TAG, "openCamera: camera button click");
+
+        String[] perms = {Manifest.permission.CAMERA, Manifest.permission.ACCESS_FINE_LOCATION};
+
+        if (EasyPermissions.hasPermissions(this,perms)){
+          //  Toast.makeText(this, "Camera Permission done", Toast.LENGTH_SHORT).show();
+        }else {
+            EasyPermissions.requestPermissions(this,"we need to permission open this app",123,perms);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode,permissions,grantResults,this);
+    }
+
+
+
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+
+        if (EasyPermissions.somePermissionPermanentlyDenied(this,perms)){
+            new AppSettingsDialog.Builder(this).build().show();
+        }
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == AppSettingsDialog.DEFAULT_SETTINGS_REQ_CODE){
+            Log.i(TAG, "onActivityResult: "+requestCode);
+            openCamera();
+            Toast.makeText(this, "onActivity", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
